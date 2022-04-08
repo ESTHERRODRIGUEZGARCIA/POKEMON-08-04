ccece8fd3121c925ede2f193a678aa2f64ced558
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-08 12:15:38 +0200

    pokemon tierra creado

---
+++ tipos/tierra.py
@@ -0,0 +1,13 @@
+#Earth Pokemon Este Pokemon va a tener un índice de defensa que va a estar entre 11 y 20.
+
+class tierra():
+    def defensa(self):
+        return random.randint(11,20)
+    
+    def __str__(self):
+        return "Tierra"
+    
+    def __del__(self):
+        print("Tierra ha sido eliminado. ")
+
+    
