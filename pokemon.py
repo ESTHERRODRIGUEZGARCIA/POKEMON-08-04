73df8fa8a6d5430040b1d23215f6e2d9efa4eab5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-08 11:42:34 +0200

    contructor creado

---
+++ pokemon.py
@@ -0,0 +1,9 @@
+class pokemon():
+    def __init__(self, id, nombre, arma, salud, ataque, defensa):
+        self.id = id
+        self.nombre = nombre
+        self.arma = arma
+        self.salud = salud
+        self.ataque = ataque
+        self.defensa = defensa
+        
