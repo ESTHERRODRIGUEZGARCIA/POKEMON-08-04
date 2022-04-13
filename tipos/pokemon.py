3ba62c05b9ec594203447c9495637e01447aa8c5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-04-13 16:25:44 +0200

    diferencio los 4 tipos de arma

--- tipos/pokemon.py
+++ tipos/pokemon.py
@@ -10,10 +10,11 @@ class pokemon():
     def __init__(self, id, nombre, arma, salud, ataque, defensa):
         self.id = id
         self.nombre = nombre
-        self.arma = arma
-        self.salud = salud
-        self.ataque = ataque
-        self.defensa = defensa
+        self.arma = arma #Puñetazo, Patada, Codazo, Cabezazo
+        self.salud = salud #entre 1 y 100
+        self.ataque = ataque #entre 1 y 10
+        self.defensa = defensa #entre 1 y 10
+
 
     def __str__(self):
         return "Pokemon: {}\nArma: {}\nSalud: {}\nAtaque: {}\nDefensa: {}".format(self.nombre, self.arma, self.salud, self.ataque, self.defensa)
